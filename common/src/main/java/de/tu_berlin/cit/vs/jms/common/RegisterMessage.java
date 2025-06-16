8667886fbb8444ca3a0f501077570f707578ffbc
Emil <emil.skorov@campus.tu-berlin.de>
2025-06-16 12:11:18 +0200

    WIP: added registrationWithBroker

--- common/src/main/java/de/tu_berlin/cit/vs/jms/common/RegisterMessage.java
+++ common/src/main/java/de/tu_berlin/cit/vs/jms/common/RegisterMessage.java
@@ -1,13 +1,17 @@
 package de.tu_berlin.cit.vs.jms.common;
 
 
+import java.math.BigDecimal;
+
 public class RegisterMessage extends BrokerMessage {
     private String clientName;
+    private BigDecimal initialAmount;
     
-    public RegisterMessage(String clientName) {
+    public RegisterMessage(String clientName, BigDecimal initialAmount) {
         super(Type.SYSTEM_REGISTER);
         
         this.clientName = clientName;
+        this.initialAmount = initialAmount;
     }
     
     public String getClientName() {
