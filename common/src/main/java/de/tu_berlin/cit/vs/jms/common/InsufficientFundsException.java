a32dfa44be7f7162622cee2d26b3809fb807979c
Emil <emil.skorov@campus.tu-berlin.de>
2025-06-16 09:58:19 +0200

    added custom Exception class for better readibility

--- common/src/main/java/de/tu_berlin/cit/vs/jms/common/InsufficientFundsException.java
+++ common/src/main/java/de/tu_berlin/cit/vs/jms/common/InsufficientFundsException.java
@@ -1,4 +1,7 @@
 package de.tu_berlin.cit.vs.jms.common;
 
-public class InsufficientFundsException {
+public class InsufficientFundsException extends Exception {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
 }
